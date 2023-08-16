0d50a5a83288691c94d10cb7ced63842af3fcb2b
Yunuen AM <yunameza@outlook.com>
2023-08-16 15:44:49 -0600

    ✨Feat: error handler  updated

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
@@ -7,5 +8,6 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use('/api/tasks',require ('./routes/tasksRoutes'))
+app.use(errorHandler)
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`)) 
